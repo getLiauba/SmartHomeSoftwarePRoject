45c4289c00920ed52ac203158b5fb651e2532e90
getLiauba <36175885+getLiauba@users.noreply.github.com>
2019-11-13 13:07:03 -0500

    Fixed the layout on the homescreen

--- app/src/main/java/com/smarthomemonitorsystem1/home_screen.java
+++ app/src/main/java/com/smarthomemonitorsystem1/home_screen.java
@@ -18,14 +18,8 @@ public class home_screen extends Fragment {
         @Nullable
         @Override
         public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.activity_home_screen, container, false);
-
-
+            return inflater.inflate(R.layout.home_screen, container, false);
+            
         }
 
-
-
-
-
     }
-
